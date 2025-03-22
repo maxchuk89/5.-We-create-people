b82911836ebef417a7efb030316862c5b3c9c5f1
maxchuk89 <maxchuk89@gmail.com>
2025-03-22 11:36:51 +0300

    Initial commit

---
+++ 5. We create people.py
@@ -0,0 +1,79 @@
+import file_operations
+import random
+from faker import Faker
+
+
+SKILLS = [
+    "Стремительный прыжок",
+    "Электрический выстрел",
+    "Ледяной удар",
+    "Стремительный удар",
+    "Кислотный взгляд",
+    "Тайный побег",
+    "Ледяной выстрел",
+    "Огненный заряд"
+]
+
+RUNIC_SYMBOLS = {
+    'а': 'а͠', 'б': 'б̋', 'в': 'в͒͠',
+    'г': 'г͒͠', 'д': 'д̋', 'е': 'е͠',
+    'ё': 'ё͒͠', 'ж': 'ж͒', 'з': 'з̋̋͠',
+    'и': 'и', 'й': 'й͒͠', 'к': 'к̋̋',
+    'л': 'л̋͠', 'м': 'м͒͠', 'н': 'н͒',
+    'о': 'о̋', 'п': 'п̋͠', 'р': 'р̋͠',
+    'с': 'с͒', 'т': 'т͒', 'у': 'у͒͠',
+    'ф': 'ф̋̋͠', 'х': 'х͒͠', 'ц': 'ц̋',
+    'ч': 'ч̋͠', 'ш': 'ш͒͠', 'щ': 'щ̋',
+    'ъ': 'ъ̋͠', 'ы': 'ы̋͠', 'ь': 'ь̋',
+    'э': 'э͒͠͠', 'ю': 'ю̋͠', 'я': 'я̋',
+    'А': 'А͠', 'Б': 'Б̋', 'В': 'В͒͠',
+    'Г': 'Г͒͠', 'Д': 'Д̋', 'Е': 'Е',
+    'Ё': 'Ё͒͠', 'Ж': 'Ж͒', 'З': 'З̋̋͠',
+    'И': 'И', 'Й': 'Й͒͠', 'К': 'К̋̋',
+    'Л': 'Л̋͠', 'М': 'М͒͠', 'Н': 'Н͒',
+    'О': 'О̋', 'П': 'П̋͠', 'Р': 'Р̋͠',
+    'С': 'С͒', 'Т': 'Т͒', 'У': 'У͒͠',
+    'Ф': 'Ф̋̋͠', 'Х': 'Х͒͠', 'Ц': 'Ц̋',
+    'Ч': 'Ч̋͠', 'Ш': 'Ш͒͠', 'Щ': 'Щ̋',
+    'Ъ': 'Ъ̋͠', 'Ы': 'Ы̋͠', 'Ь': 'Ь̋',
+    'Э': 'Э͒͠͠', 'Ю': 'Ю̋͠', 'Я': 'Я̋',
+    ' ': ' '
+}
+
+
+def main():
+    fake = Faker("ru_RU")
+    for i in range(1, 11):
+        s_random_number = random.randint(3, 18)
+        a_random_number = random.randint(3, 18)
+        e_random_number = random.randint(3, 18)
+        i_random_number = random.randint(3, 18)
+        l_random_number = random.randint(3, 18)
+
+        random_skill_1, random_skill_2, random_skill_3 = random.sample(SKILLS, 3)
+
+        for letter, runic in RUNIC_SYMBOLS.items():
+            random_skill_1 = random_skill_1.replace(letter, runic)
+            random_skill_2 = random_skill_2.replace(letter, runic)
+            random_skill_3 = random_skill_3.replace(letter, runic)
+
+        context = {
+            "first_name": fake.first_name(),
+            "last_name": fake.last_name(),
+            "job": fake.job(),
+            "town": fake.city(),
+            "strength": s_random_number,
+            "agility": a_random_number,
+            "endurance": e_random_number,
+            "intelligence": i_random_number,
+            "luck": l_random_number,
+            "skill_1": random_skill_1,
+            "skill_2": random_skill_2,
+            "skill_3": random_skill_3
+        }
+
+        output_svgname = "output/svg/result_{}.svg".format(i)
+        file_operations.render_template("src/charsheet.svg", output_svgname, context)
+
+if __name__ == '__main__':
+    main()
