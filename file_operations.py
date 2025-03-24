6e36d9f44dde4da860166073f52ec23a9af9e23d
maxchuk89 <maxchuk89@gmail.com>
2025-03-24 19:32:33 +0300

    Улучшил код
    1 Создает папку для карточек
    2 рунические символы по правилам PEP8.

--- file_operations.py
+++ file_operations.py
@@ -1,18 +1,18 @@
-VERSION = "1.0"
+import os
+
+VERSION = "1.1"
 
 def read_file(filename):
     with open(filename, encoding='utf8') as file_:
         return file_.read()
 
 def write_to_file(filename, content):
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
     with open(filename, 'w', encoding='utf8') as file_:
         return file_.write(content)
 
-
 def render_template(template_path, output_path, context):
     content = read_file(template_path)
-
     for key, value in context.items():
         content = content.replace('{%s}' % key, str(value))
-
     write_to_file(output_path, content)
